411b19d063f389ac8bd36451f59fb714aa4a8b49
Andrei Berenda <berenda.andrey@gmail.com>
2025-04-23 18:15:42 +0200

    Add create_user function

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class User:
+  first_name: str
+  last_name: str
 
 
 def awesome_function():
   return 'I am the best one'
+
+
+
+def create_user(first_name: str, last_name: str) -> User:
+  return User(first_name, last_name)
